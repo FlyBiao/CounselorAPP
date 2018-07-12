bc799eb22f9a9a6e81aad2e936910fbfdf5b35f4
FlyBiao <fengguangbiao@163.com>
2018-07-12 17:57:42 +0800

    library
    library

---
+++ CounselorAPP/library/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="518ab208-bf0c-44f0-a609-037b70180b57")
+public class DataBindingInfo {}
---
+++ CounselorAPP/library/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="20352200-089a-44de-8aad-26fb6dfde6c9")
+public class DataBindingInfo {}
